4f445723bb69810445bae45f6509469596749c96
Tommy Li <tommyli3318@gmail.com>
2020-04-14 00:25:28 -0700

    core functionality

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ addEventListener('fetch', event => {
  * Respond with hello worker text
  * @param {Request} request
  */
-
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  const resp = await fetch('https://cfw-takehome.developers.workers.dev/api/variants');
+  const respJson = await resp.json();
+  const variantsArray = respJson.variants; // Contains two URLs
+
+  var randomIndex = Math.floor(Math.random() * 2); // Choose 0 or 1
+  const response = await fetch(variantsArray[randomIndex]); // response.body is ReadableStream
+  return new Response(response.body); // Return the ReadableStream
 }
