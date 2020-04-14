7b147ac95dcd51efe0a7fbffe9d451f41031acba
Tommy Li <tommyli3318@gmail.com>
2020-04-14 10:38:20 -0700

    extra credit 1

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
+var variantIndex; // global scope
+
 /**
  * Respond with hello worker text
  * @param {Request} request
@@ -11,7 +13,39 @@ async function handleRequest(request) {
   const respJson = await resp.json();
   const variantsArray = respJson.variants; // Contains two URLs
 
-  var randomIndex = Math.floor(Math.random() * 2); // Choose 0 or 1
-  const response = await fetch(variantsArray[randomIndex]); // response.body is ReadableStream
-  return new Response(response.body); // Return the ReadableStream
+  variantIndex = Math.floor(Math.random() * 2); // Choose 0 or 1
+  let response = await fetch(variantsArray[variantIndex]); // response.body is ReadableStream
+
+  newResponse = replaceHTML(response);
+  return new Response(newResponse.body); // Return the ReadableStream
+}
+
+function replaceHTML(response) {
+  // Extra credit: Changing copy/URLs
+  return new HTMLRewriter()
+    .on('*', new ElementHandler())
+    .transform(response);
+}
+
+class ElementHandler {
+  element(element) {
+    switch(element.tagName) {
+      case "title":
+        element.setInnerContent("Tommy's CloudFlare Application");
+        break;
+      
+      case "h1":
+        element.setInnerContent(`My Custom Variant ${variantIndex+1}`);
+        break;
+      
+      case "p":
+        element.setInnerContent("Check out my amazing personal website :)");
+        break;
+
+      case "a":
+        element.setAttribute("href", "https://tommyli3318.github.io/");
+        element.setInnerContent("https://tommyli3318.github.io/");
+        break;
+    }
+  }
 }
